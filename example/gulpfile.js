d62ec35acd53fe1b4d7cf78a8cdf9e0c6658c503
p.knapik <pawel.knapik@gmail.com>
2015-08-07 13:39:43 +0200

    example project

---
+++ example/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var hbsmaster = require('gulp-handlebars-master');
+var rename = require('gulp-rename');
+var fs = require('fs');
+
+gulp.task('handlebars', function() {
+
+    var templatedata = JSON.parse(fs.readFileSync('./src/data.json'));
+    var options = {
+	batch : [ './src/partials/' ]
+    };
+    
+    gulp.src('./src/pages/*.hbs')
+	.pipe( hbsmaster('./src/master.hbs', templatedata, options ))
+      .pipe( rename( function(path){
+        path.extname = '.html';
+      }))
+      .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['handlebars']);
